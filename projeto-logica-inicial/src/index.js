849cc452ebb4141508402adcfa7dfb31e322f88c
oxlucasp <lucas-lukasc@hotmail.com>
2023-10-30 11:02:45 -0300

    finalizado, real

--- projeto-logica-inicial/src/index.js
+++ projeto-logica-inicial/src/index.js
@@ -1,27 +1,34 @@
-//adicionado variáveis
-const heroi = "perro en llamas"
-let heroiXP = 11000
+const heroi = "perro en llamas";
+let heroiXP = 11000;
 let nivel;
+let mensagem;
 
-//Estrutura de decisão, conforme XP
-
+// Estrutura de decisão, conforme heroiXP
 if (heroiXP < 1000) {
-    nivel = "Ferro";
+  nivel = "Ferro";
+  mensagem = "Parabéns, você desbloqueou a picareta";
 } else if (heroiXP >= 1000 && heroiXP <= 2000) {
-    nivel = "Bronze";
+  nivel = "Bronze";
+  mensagem = "Parabéns, agora você tem uma espada";
 } else if (heroiXP >= 2001 && heroiXP <= 5000) {
-    nivel = "Prata";
+  nivel = "Prata";
+  mensagem = "Parabéns, você desbloqueou a adaga";
 } else if (heroiXP >= 6001 && heroiXP <= 7000) {
-    nivel = "Ouro";
+  nivel = "Ouro";
+  mensagem = "Parabéns, você desbloqueou a skin lendária";
 } else if (heroiXP >= 7001 && heroiXP <= 8000) {
-    nivel = "Platina";
+  nivel = "Platina";
+  mensagem = "Parabéns, você agora pode ter defesa, ataque e magia aumentada em 10.000";
 } else if (heroiXP >= 8001 && heroiXP <= 9000) {
-    nivel = "Ascendente";
+  nivel = "Ascendente";
+  mensagem = "Parabéns, agora você receberá menos dano de picareta, espada e adagas";
 } else if (heroiXP >= 9001 && heroiXP <= 10000) {
-    nivel = "Imortal";
+  nivel = "Imortal";
+  mensagem = "Parabéns, sua arma principal causará mais dano, e lançará chamas ao bater em inimigos";
 } else {
-    nivel = "Radiante";
+  nivel = "Radiante";
+  mensagem = "Parabéns, você está no auge do poder, sua arma principal causará mais dano e lançará chamas ao bater em inimigos!";
 }
 
-//mensagem de saída
-console.log("O heroi de nome " + heroi + "está no nível de " + nivel)
+// Mensagem de saída
+console.log("O heroi de nome " + heroi + " está no nível de " + nivel + ". " + mensagem);
