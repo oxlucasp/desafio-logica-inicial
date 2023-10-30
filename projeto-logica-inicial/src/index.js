addc3f794a4f125d40184f6cd86823a8153dd324
oxlucasp <lucas-lukasc@hotmail.com>
2023-10-30 10:47:48 -0300

    finalizado

--- projeto-logica-inicial/src/index.js
+++ projeto-logica-inicial/src/index.js
@@ -1,1 +1,27 @@
-console.log("ok vamos lá")  
+//adicionado variáveis
+const heroi = "perro en llamas"
+let heroiXP = 11000
+let nivel;
+
+//Estrutura de decisão, conforme XP
+
+if (heroiXP < 1000) {
+    nivel = "Ferro";
+} else if (heroiXP >= 1000 && heroiXP <= 2000) {
+    nivel = "Bronze";
+} else if (heroiXP >= 2001 && heroiXP <= 5000) {
+    nivel = "Prata";
+} else if (heroiXP >= 6001 && heroiXP <= 7000) {
+    nivel = "Ouro";
+} else if (heroiXP >= 7001 && heroiXP <= 8000) {
+    nivel = "Platina";
+} else if (heroiXP >= 8001 && heroiXP <= 9000) {
+    nivel = "Ascendente";
+} else if (heroiXP >= 9001 && heroiXP <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+//mensagem de saída
+console.log("O heroi de nome " + heroi + "está no nível de " + nivel)
